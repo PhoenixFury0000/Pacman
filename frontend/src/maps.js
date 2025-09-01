16847ef14b2bc05329cdae4faa137d6870fd38a0
𝚸𝚮𝚯𝚵𝚴𝚰𝚾 <piyushguptaofficial07042009@gmail.com>
2025-09-02 00:04:22 +0530

    Update maps.js

--- frontend/src/maps.js
+++ frontend/src/maps.js
@@ -1,1 +1,12 @@
-.
+export const maps = [
+  [
+    "WWWWWWWWWWWW",
+    "W..........W",
+    "W.WWW.WWW.WW",
+    "W.W......W.W",
+    "W.WWW.WWW.WW",
+    "W..........W",
+    "WWWWWWWWWWWW",
+  ],
+  // ... add 19 more maps here
+];
