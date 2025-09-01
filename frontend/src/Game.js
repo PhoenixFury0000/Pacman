bcbebe08d0b727140c9df3223ea546c686c6556d
𝚸𝚮𝚯𝚵𝚴𝚰𝚾 <piyushguptaofficial07042009@gmail.com>
2025-09-02 00:04:04 +0530

    Update Game.js

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,1 +1,10 @@
-.
+// inside Game.js
+useEffect(() => {
+  if (player) {
+    fetch(`http://localhost:5000/api/players/${player._id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ score, level: level + 1, lives }),
+    });
+  }
+}, [score, lives, level]);
